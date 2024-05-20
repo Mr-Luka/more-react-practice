14d59e2cfd71cf22ceea8fd212c60efb7e280ca4
Luka <luka.luka.luka93@gmail.com>
2024-05-20 16:16:56 -0700

    checkout

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import NameTag from "./nameTag.js";
 
 import "./index.css";
 
+const renderItem = charge => (
+  <p key={charge.id}>
+    <span> {charge.name}</span>
+    <span> {charge.price}</span>
+  </p>
+)
+
+const Receipt = props => {
+  const receiptItems = props.charges.map(renderItem);
+  return <div>{receiptItems}</div>
+}
 
 const App = () => {
   const whatever = "Marko"
