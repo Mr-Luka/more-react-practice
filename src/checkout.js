7001d3d09ab58d5d59340e1301769263e8acea35
Luka <luka.luka.luka93@gmail.com>
2024-05-20 16:44:44 -0700

    no checkout

--- src/checkout.js
+++
@@ -1,7 +0,0 @@
-
-const chargesData = [
-    {name: "Mint Tea", price: 4.32, id: "0019"},
-    {name: "Tagine", price: 9.32, id: "0001"},
-    {name: "Gelato", price: 3.61, id: "0003"}
-]
-export default chargesData;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NameTag from "./nameTag.js";
+
 import "./index.css";
 
-const renderItem = charge => (
-  <p key={charge.id}>
-    <span> {charge.name}</span>
-    <span> {charge.price}</span> 
-  </p>
-)
-
-export const Receipt = props => {
-  const receiptItems = props.charges.map(renderItem);
-  return <div>{receiptItems}</div>
-}
 
 
 const App = () => {
@@ -24,7 +14,6 @@ const App = () => {
       <NameTag name = "Luka"/>
       <NameTag name = {whatever}/>
       <NameTag name = "Slavica"/>
-      <NameTag name = "Bojana"/>
     </h1>
   </div>
 )
