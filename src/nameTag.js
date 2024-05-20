74201dc7b63dd0048515e834d46d7fbf713084fe
Luka <luka.luka.luka93@gmail.com>
2024-05-20 13:00:30 -0700

    nameTag js folder added with nameTag function

---
+++ src/nameTag.js
@@ -0,0 +1,10 @@
+
+const NameTag = () => (
+    <div className ="name-tag">
+        <h3 className = "title">Hello</h3>
+        <p className = "subtitle">my name is</p>
+        <h2 className = "name">Luka</h2>
+    </div>
+)
+
+export default NameTag
